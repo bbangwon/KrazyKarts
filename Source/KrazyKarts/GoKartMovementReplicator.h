3a4e746e585b0926bff7dd101c2e09c6a9a04ed2
youngil <youngil@placeb.com>
2024-04-23 08:55:15 +0900

    속도를 위한 FMath::CubicInterp

--- Source/KrazyKarts/GoKartMovementReplicator.h
+++ Source/KrazyKarts/GoKartMovementReplicator.h
@@ -67,6 +67,7 @@ private:
 	float ClientTimeSinceUpdate;
 	float ClientTimeBetweenLastUpdates;
 	FTransform ClientStartTransform;
+	FVector ClientStartVelocity;
 
 	UPROPERTY()
 	UGoKartMovementComponent* MovementComponent;
